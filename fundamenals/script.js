9948c117ae71be7da1079138daac8a24b6ca1309
Kyrylo Lvov <70720713+kyrylolvov@users.noreply.github.com>
2020-11-25 23:59:38 +0200

    Update script.js

--- fundamenals/script.js
+++ fundamenals/script.js
@@ -1,1 +1,35 @@
+`use strict`;
 
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log(`I can drive`);
+
+function logger() {
+    console.log(`My name is Kyrylo`);
+}
+
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+const appleJuice = fruitProcessor(5, 2);
+console.log(appleJuice);
+
+console.log(fruitProcessor(3, 4));
+
+function calcAge(birthYear) {
+    return 2020 - birthYear;
+}
+
+const age = calcAge(2002);
+console.log(age);
+
+const calcAgeNew = function (birthYear) {
+    return 2020 - birthYear;
+}
+console.log(calcAgeNew(2002));
